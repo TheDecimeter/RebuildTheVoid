e48fa6a05b2603273aa63860beb5e02c4704e9e2
Daniel Williams <spawnofthepilgrims@googlemail.com>
2020-10-24 18:27:43 -0600

    Added embankment to level

--- Assets/Scripts/Level/LevelController.cs
+++ Assets/Scripts/Level/LevelController.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class LevelController : MonoBehaviour
 {
-    public Tile tileTemplate;
+    public Tile TileTemplate;
+    public Tile Embankment;
 
     public float tileSize = 10;
+    private static float tileSizeStat;
 
     Tile[][] map;
 
     // Start is called before the first frame update
     void Start()
     {
+        tileSizeStat = tileSize;
         GenerateLevel(7, 5);
+        AddEmbankement();
     }
 
     private void GenerateLevel(int length, int width)
@@ -30,38 +34,37 @@ public class LevelController : MonoBehaviour
         for (int x = 0; x < length; ++x)
             for (int y = 0; y < width; ++y)
             {
-                GameObject g = Instantiate(tileTemplate.gameObject);
-                SetPhysicalLocation(g.transform, x, y);
-                map[x][y] = g.GetComponent<Tile>();
+                GameObject g = Instantiate(TileTemplate.gameObject);
+                Tile t= g.GetComponent<Tile>();
+                map[x][y] = t;
+                t.SetPos(x, y);
             }
     }
 
-    //private void AddEmbankement()
-    //{
-    //    for(int i=0; i<map[0].Length; ++i)
-    //    {
-    //        UpdateTile
-    //    }
-            
-    //}
+    private void AddEmbankement()
+    {
+        for (int i = 0; i < map[0].Length; ++i)
+        {
+            map[0][i].Add(Embankment);
+        }
+        for (int i = 0; i < map[0].Length; ++i)
+        {
+            map[map.Length-1][i].Add(Embankment);
+        }
+    }
 
     private void UpdateTile(Tile tile, int x, int y)
     {
-        print("Level Controller add tile " + x + " " + y);
-        if (x >= 0 && x < map.Length && y >= 0 && y < map[0].Length)
-        {
-            if (map[x][y] == null)
-            {
-                GameObject g = Instantiate(tile.gameObject);
-                SetPhysicalLocation(g.transform, x, y);
-                map[x][y] = g.GetComponent<Tile>();
-            }
+        if (!(x >= 0 && x < map.Length && y >= 0 && y < map[0].Length))
+            return;
 
-        }
+        print("Level Controller add tile " + x + " " + y);
+
     }
-    private void SetPhysicalLocation(Transform t, int x, int y)
+
+    public static Vector3 PhysicalLocation(int x, int y)
     {
-        t.position = new Vector3(x * tileSize, 0, y * tileSize);
+        return new Vector3(x * tileSizeStat, 0, y * tileSizeStat);
     }
 
     // Update is called once per frame
--- Assets/Scripts/Level/Tile.cs
+++ Assets/Scripts/Level/Tile.cs
@@ -4,19 +4,64 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
+    public GameObject floor;
+    public GameObject pillar;
+    public bool addon = false;
+
+    public int StackSize { get; protected set; }
+
+    private int tileHeight = 1, x, y;
+
+    private List<Tile> additions = new List<Tile>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StackSize = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetPos(int x, int y)
     {
-        
+        this.transform.position = LevelController.PhysicalLocation(x, y);
+        this.x = x;
+        this.y = y;
     }
 
-    public void Combine(Tile newTile)
+    public void UpdateHeight()
     {
+        Vector3 pos = new Vector3(pillar.transform.position.x, StackSize * tileHeight, pillar.transform.position.z);
+        floor.transform.localScale = new Vector3(10, tileHeight * StackSize, 10);
+        floor.transform.position = new Vector3(floor.transform.position.x, StackSize/2 * tileHeight, floor.transform.position.z);
+
+        pillar.transform.position = pos;
+
+        foreach (Tile t in additions)
+        {
+            t.transform.position = pos;
+        }
+    }
+
+    public void Add(Tile tile)
+    {
+        if (!tile.addon)
+        {
+            if (StackSize == 0)
+            {
+                floor.gameObject.SetActive(true);
+                pillar.gameObject.SetActive(true);
+            }
+            StackSize++;
+            
+        }
+        GameObject g = Instantiate(tile.gameObject);
+        g.transform.position = LevelController.PhysicalLocation(x, y);
+        additions.Add(g.GetComponent<Tile>());
+
+        updateHeight();
+    }
+
+    private void updateHeight()
+    {
+        
     }
 }
