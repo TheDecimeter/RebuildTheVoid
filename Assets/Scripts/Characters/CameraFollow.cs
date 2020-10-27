402d2afa53c19644f9ac65eae18ef10e1712ba24
Daniel Williams <spawnofthepilgrims@googlemail.com>
2020-10-27 16:37:20 -0600

    Added follow camera

---
+++ Assets/Scripts/Characters/CameraFollow.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform Target;
+    public Transform Camera;
+    public Transform Calculator;
+    public float MaxFollowDistance;
+    public float MinFollowDistance;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Follow();
+        Point();
+    }
+
+    private void Follow()
+    {
+        Vector3 dir = transform.position - Target.position;
+        if (dir.magnitude > MaxFollowDistance)
+        {
+            dir = new Vector3(dir.x, 0, dir.z).normalized;
+            transform.position = Target.position + dir * MaxFollowDistance;
+        }
+        else if (dir.magnitude < MinFollowDistance)
+        {
+            dir = new Vector3(dir.x, 0, dir.z).normalized;
+            transform.position = Target.position + dir * MinFollowDistance;
+        }
+    }
+
+    private void Point()
+    {
+        //Camera.LookAt(Target);
+        Calculator.LookAt(Target);
+        Camera.rotation = Quaternion.Slerp(Camera.rotation, Calculator.rotation, 0.9f);
+    }
+}
